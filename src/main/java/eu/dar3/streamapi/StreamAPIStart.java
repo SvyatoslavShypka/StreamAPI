1c33e7e493e8d63e5e7c9705e4b0eedc5866970b
Svyatoslav Shypka <svyat200909@gmail.com>
2022-02-06 07:14:53 +0100

    Old Filter by specialties done

--- src/main/java/eu/dar3/streamapi/StreamAPIStart.java
+++ src/main/java/eu/dar3/streamapi/StreamAPIStart.java
@@ -1,5 +1,44 @@
 package eu.dar3.streamapi;
 
-public class StreamAPIStart {
+import eu.dar3.streamapi.model.Specialist;
+import eu.dar3.streamapi.model.Specialty;
+import eu.dar3.streamapi.util.OldWayUtil;
 
+import java.util.List;
+
+public class StreamAPIStart {
+    public static void main(String[] args) {
+
+        List<Specialist> specialistsOld = OldWayUtil.getSpecialists();
+        System.out.println("Old");
+        System.out.println(specialistsOld);
+
+
+        // Filter by specialties
+        for (Specialty specialty: Specialty.values()
+             ) {
+        List<Specialist> filteredListBySpecialty = OldWayUtil.filterBySpecialty(specialistsOld, specialty);
+        System.out.println("Old filter by specialties - " + specialty + ":");
+        System.out.println(filteredListBySpecialty);
+        }
+
+        // Sorting
+
+
+        // Max salary
+
+
+        // Min salary
+
+
+        // Grouping
+
+
+        // Any specialist
+
+
+        // No one with salary more then
+
+
+    }
 }
---
+++ src/main/java/eu/dar3/streamapi/util/OldWayUtil.java
@@ -0,0 +1,52 @@
+package eu.dar3.streamapi.util;
+
+import eu.dar3.streamapi.model.Specialist;
+import eu.dar3.streamapi.model.Specialty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OldWayUtil {
+
+    // Get initial specialist list
+    public static List<Specialist> getSpecialists() {
+        List<Specialist> list = new ArrayList<>();
+        list.add(new Specialist("Petro Petrenko", 8000, Specialty.DEVOPS));
+        list.add(new Specialist("Igor Igorenko", 80000, Specialty.DIRECTOR));
+        list.add(new Specialist("Taras Tarasenko", 15000, Specialty.ENGINEER));
+        list.add(new Specialist("Ivan Ivanenko", 10000, Specialty.ENGINEER));
+        list.add(new Specialist("Vasyl Vasylenko", 10000, Specialty.DEVOPS));
+        list.add(new Specialist("Stepan Stepanenko", 9000, Specialty.DEVOPS));
+
+        return list;
+    }
+
+    // Filter by speciality
+    public static List<Specialist> filterBySpecialty(List<Specialist> list, Specialty specialty) {
+        List<Specialist> result = new ArrayList<>();
+        for (Specialist specialist: list
+             ) {
+            if (specialist.getSpecialty().equals(specialty)) {
+                result.add(specialist);
+            }
+        }
+        return result;
+    }
+
+
+    // Checking for matches
+
+
+    // Check whether there is any Speciality
+
+
+    // Check whether all have salary more than
+
+
+    // Print
+
+
+    // Group specialists by specialty
+
+
+}
